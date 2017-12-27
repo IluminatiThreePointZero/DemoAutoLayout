0bbfc73573ab834b343eac21a1308beae09614fa
Juan Antonio Perez Clemente <juaniyoalm@gmail.com>
2017-12-27 16:33:33 +0100

    Initial Commit

---
+++ DemoAutoLayoutV2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoAutoLayoutV2
+//
+//  Created by Juan Antonio Perez Clemente on 27/12/17.
+//  Copyright © 2017 Juan Pérez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
